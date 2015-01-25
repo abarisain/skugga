3fc1e21e61277f809d201073659fc0985f0a68d3
Arnaud Barisain-Monrose <abarisain@gmail.com>
2015-01-25 19:01:15 +0100

    Statusbar Icon accepts D&D

--- skugga/AppDelegate.swift
+++ skugga/AppDelegate.swift
@@ -9,25 +9,47 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate, NSDraggingDestination {
 
     @IBOutlet weak var window: NSWindow!
 
     var statusItem: NSStatusItem!
 
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
+    func applicationDidFinishLaunching(aNotification: NSNotification)
+    {
         // Insert code here to initialize your application
-        statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1); // Linker error : Use -1 instead of NSVariableStatusItemLength
-        var iconImage: NSImage! = NSImage(named: "Menubar_Idle");
-        iconImage.setTemplate(true);
-        statusItem.image = iconImage;
-
+        initStatusItem();
     }
 
-    func applicationWillTerminate(aNotification: NSNotification) {
+    func applicationWillTerminate(aNotification: NSNotification)
+    {
         // Insert code here to tear down your application
     }
 
-
+    // MARK: NSDraggingDestination protocol methods
+    func draggingEntered(sender: NSDraggingInfo) -> NSDragOperation
+    {
+        return NSDragOperation.Copy;
+    }
+    
+    func performDragOperation(sender: NSDraggingInfo) -> Bool
+    {
+        return true;
+    }
+    
+    // MARK: Statusbar Icon
+    private func initStatusItem()
+    {
+        if (statusItem == nil)
+        {
+            statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1); // Linker error : Use -1 instead of NSVariableStatusItemLength
+            var iconImage: NSImage! = NSImage(named: "Menubar_Idle");
+            iconImage.setTemplate(true);
+            statusItem.image = iconImage;
+            var button = statusItem.button;
+            button?.window?.registerForDraggedTypes([NSURLPboardType]);
+            button?.window?.delegate = self;
+        }
+    }
 }
 
