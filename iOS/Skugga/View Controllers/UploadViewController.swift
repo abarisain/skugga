ba8ef0c89aa6ab0fb8e1b6650b42ac9d9f7fc241
Arnaud Barisain-Monrose <abarisain@gmail.com>
2015-02-14 18:08:21 +0100

    Refresh file list after uploading

--- iOS/Skugga/View Controllers/UploadViewController.swift
+++ iOS/Skugga/View Controllers/UploadViewController.swift
@@ -44,9 +44,11 @@ class UploadViewController : UIViewController
             mimetype: "image/jpeg",
             progress: { (bytesSent:Int64, bytesToSend:Int64) -> Void in
                 dispatch_sync(dispatch_get_main_queue(), { () -> Void in
+                    
                     self.progressView.progress = Float(Double(bytesSent) / Double(bytesToSend));
                 });
             }, success: { (data: [NSObject : AnyObject]) -> Void in
+                
                 var url = data["name"] as NSString;
                 url = Configuration.endpoint + url;
                 
@@ -58,7 +60,12 @@ class UploadViewController : UIViewController
                 }));
                 alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { (action: UIAlertAction!) -> () in self.dismissViewControllerAnimated(true, completion: nil) }));
                 self.presentViewController(alert, animated: true, completion: nil);
+                
+                // Start refreshing the file list, and hope it will be fresh for the RemoteFile list view
+                RemoteFileDatabaseHelper.refreshFromServer();
+                
             }, failure: { (error: NSError) -> Void in
+                
                 let alert = UIAlertController(title: "Error", message: "Couldn't upload image : \(error) \(error.userInfo)", preferredStyle: .Alert);
                 alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: { (action: UIAlertAction!) -> () in self.dismissViewControllerAnimated(true, completion: nil) }));
                 self.presentViewController(alert, animated: true, completion: nil);
