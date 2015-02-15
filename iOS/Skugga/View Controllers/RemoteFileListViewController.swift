2a1fe367ead4a3d10fd3c95123217d2e0b578bbd
Arnaud Barisain-Monrose <abarisain@gmail.com>
2015-02-15 19:45:42 +0100

    I feel bad

--- iOS/Skugga/View Controllers/RemoteFileListViewController.swift
+++ iOS/Skugga/View Controllers/RemoteFileListViewController.swift
@@ -132,6 +132,7 @@ class RemoteFileTableViewCell : UITableViewCell
     {
         filenameLabel.text = remoteFile.filename
         dateLabel.text = remoteFile.uploadDate.description
+        //FIXME : Terrible, terrible method. FIX IT DAMNIT
         fileImageView.sd_setImageWithURL(NSURL(string: Configuration.endpoint + remoteFile.url + "?w=96&h=96")!)
     }
 }
