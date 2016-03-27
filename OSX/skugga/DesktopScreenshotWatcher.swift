63ca297c0ef41b89254cb3f9c938abe77bed7c9d
Arnaud Barisain-Monrose <abarisain@gmail.com>
2016-03-27 12:11:52 +0200

    FS Watcher basic checks

--- OSX/skugga/DesktopScreenshotWatcher.swift
+++ OSX/skugga/DesktopScreenshotWatcher.swift
@@ -14,7 +14,7 @@ public class DesktopScreenshotWatcher: FilesystemEventListenerDelegate {
     
     let fileManager = NSFileManager.defaultManager()
     
-    var screenshotFolder = ("~/Desktop" as NSString).stringByExpandingTildeInPath
+    var screenshotFolder = ("~/Desktop" as NSString).stringByStandardizingPath
     
     //TODO (arnaud): Read the capture extension https://github.com/abarisain/scrup/blob/2ffd01b52b8ada3e698d4aee7e863cb0351eb36c/src/DPAppDelegate.m#L121
     var screenshotExtension = ".png"
@@ -41,14 +41,19 @@ public class DesktopScreenshotWatcher: FilesystemEventListenerDelegate {
         //  - NSFileExtendedAttributes has com.apple.metadata:kMDItemIsScreenCapture
         // We'll also read the event flags to filter unwanted fs events, and ignore anything that doesn't have the right file suffix
         
-        let createdFlag = UInt32(kFSEventStreamEventFlagItemCreated)
+        let renamedFlag = UInt32(kFSEventStreamEventFlagItemRenamed)
+        let xattrsModifiedFlag = UInt32(kFSEventStreamEventFlagItemXattrMod)
         
         for event in events {
-            guard event.flags & createdFlag == 0 else { continue }
-            guard !event.path.hasSuffix(screenshotExtension) else { continue }
+            guard event.flags & renamedFlag != 0 else { continue }
+            // We get two events per screenshot. Ignore the xattrs one
+            guard event.flags & xattrsModifiedFlag == 0 else { continue }
+            guard event.path.hasSuffix(screenshotExtension) else { continue }
+            // Ignore subfolders
+            guard (event.path as NSString).stringByDeletingLastPathComponent == screenshotFolder else { continue }
             
             //TODO: Remove this
-            print("DEBUG: Candidate file \(event)")
+            print("DEBUG: Candidate file \(event) \((event.path as NSString).stringByDeletingLastPathComponent)")
         }
     }
 }
