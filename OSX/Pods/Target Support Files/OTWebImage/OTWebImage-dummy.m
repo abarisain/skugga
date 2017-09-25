5d557cb3158b1a99530034a168e6bd7e61f3bc5d
Arnaud Barisain-Monrose <abarisain@gmail.com>
2017-09-25 13:53:30 +0200

    Refactor all projects into one workspace

--- OSX/Pods/Target Support Files/OTWebImage/OTWebImage-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_OTWebImage : NSObject
-@end
-@implementation PodsDummy_OTWebImage
-@end
--- iOS/Pods/Target Support Files/Pods-Skugga-iOS/Pods-Skugga-iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Skugga_iOS : NSObject
-@end
-@implementation PodsDummy_Pods_Skugga_iOS
-@end
